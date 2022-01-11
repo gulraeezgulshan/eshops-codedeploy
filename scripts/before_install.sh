a5de0f8becfbfd0a9fd5926ed3c81b2198b7cb9e
Gulraiz Gulshan <gulraizgulshan@gmail.com>
2022-01-11 20:32:54 +0500

    remove error from script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,18 +7,20 @@ nvm install node
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
+
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
   cd ${DIR}
-  cat > .env <<message
+  ##here document
+cat > .env <<message
 
   API_URL=/api/v1
   PORT=3000
   CONNECTION_STRING=mongodb+srv://aptech:123456789abc@mydemocluster.6ihvf.mongodb.net/eshops?retryWrites=true&w=majority
   secret=this-is-my-secret
   
-  message
+message
 fi
